ce39640ad992415aa3f363b724deb95e416a1504
blackhat-7 <palshaunak7@gmail.com>
2026-06-27 15:00:21 +0530

    Restore subagents agent widget rendering

--- pi.nix
+++ pi.nix
@@ -178,7 +178,7 @@ let
     ${writePiSettings}
     "$npm_bin/pi" update --extensions
     ${pkgs.nodejs_24}/bin/node ${./files/patch-pi-subagents-mouse.js}
-    ${pkgs.nodejs_24}/bin/node ${./files/patch-pi-subagents-footer-click.js}
+    ${pkgs.nodejs_24}/bin/node ${./files/restore-pi-subagents-agent-widget.js}
   '';
 in
 {
