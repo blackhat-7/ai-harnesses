82b84c9ca6366452e7699daf7012e3b66101b3fd
blackhat-7 <palshaunak7@gmail.com>
2026-07-12 16:12:11 +0530

    chore: pin npm:@codexstar/pi-listen dependency to version 7.2.0

--- pi.nix
+++ pi.nix
@@ -66,7 +66,7 @@ let
       "git:github.com/blackhat-7/pi-dynamic-workflows@permission-prompts"
       "npm:pi-vim"
       "npm:pi-hermes-memory"
-      "npm:@codexstar/pi-listen"
+      "npm:@codexstar/pi-listen@7.2.0"
     ]
   );
 
